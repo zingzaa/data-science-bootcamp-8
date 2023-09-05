dda4d5a3d76b55c4f34e80c0331e7aa06d83f4aa
zingzaa <zingzaa@gmail.com>
2023-09-05 02:08:22 +0000

    add second plot

--- data_viz.r
+++ data_viz.r
@@ -3,5 +3,11 @@
 
 library(ggplots)
 
+# first plot
 ggplot(diamonds, aes(carat, price)) +
   geom_point ()
+
+# second plot
+ggplot(diamonds, aes(carat, price)) +
+  geom_point () +
+  geom_smooth ()
